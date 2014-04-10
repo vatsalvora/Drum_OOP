5634d7e983a82457ec41fa98d82454594d967ff8
devan <devanp92@ufl.edu>
2014-04-10 17:34:11 -0400

    Adding some state classes

--- src/model/State.java
+++ src/model/State.java
@@ -11,4 +11,49 @@ public interface State {
 
     public StateType getCurrentState();
 
+    public void incorrectKeyPressed();
+
+    public void keyPressed1();
+
+    public void keyPressed2();
+
+    public void keyPressed3();
+
+    public void keyPressed7();
+
+    public void keyPressed8();
+
+    public void keyPressed9();
+
+    public void keyPressedTab();
+
+    public void keyPressedR();
+
+    public void keyPressedP();
+
+    public void keyPressedV();
+
+    public void keyPressedI();
+
+    public void keyPressedX();
+
+    public void keyPressedA();
+
+    public void keyPressedESC();
+
+    public void keyPressedF();
+
+    public void keyPressedU();
+
+    public void keyPressedW();
+
+    public void keyPressedE();
+
+    public void keyPressed4();
+
+    public void keyPressed6();
+
+    public void keyPressedEnter();
+
+
 }
