ea6bec83f0524f2c2f9f2377cbedfd9c16caf2a0
devan <devanp92@ufl.edu>
2014-04-10 17:45:19 -0400

    it compiles!!!

--- src/model/Replay.java
+++ src/model/Replay.java
@@ -3,20 +3,131 @@ package model;
 /**
  * Created by devan on 4/9/14.
  */
-public class Replay extends State {
+public class Replay implements State {
+
 
     @Override
-    public void acceptInput(char c) {
-        switch(c){
-            case 'R':
-                //restart
-                break;
-            case '4':
-                //go back
-                break;
-            case '6':
-                //go forward
-                break;
-        }
+    public void changeCurrentState(State state) {
+
+    }
+
+    @Override
+    public void changeState(StateType stateType) {
+
+    }
+
+    @Override
+    public StateType getCurrentState() {
+        return null;
+    }
+
+    @Override
+    public void incorrectKeyPressed() {
+
+    }
+
+    @Override
+    public void keyPressed1() {
+
+    }
+
+    @Override
+    public void keyPressed2() {
+
+    }
+
+    @Override
+    public void keyPressed3() {
+
+    }
+
+    @Override
+    public void keyPressed7() {
+
+    }
+
+    @Override
+    public void keyPressed8() {
+
+    }
+
+    @Override
+    public void keyPressed9() {
+
+    }
+
+    @Override
+    public void keyPressedTab() {
+
+    }
+
+    @Override
+    public void keyPressedR() {
+
+    }
+
+    @Override
+    public void keyPressedP() {
+
+    }
+
+    @Override
+    public void keyPressedV() {
+
+    }
+
+    @Override
+    public void keyPressedI() {
+
+    }
+
+    @Override
+    public void keyPressedX() {
+
+    }
+
+    @Override
+    public void keyPressedA() {
+
+    }
+
+    @Override
+    public void keyPressedESC() {
+
+    }
+
+    @Override
+    public void keyPressedF() {
+
+    }
+
+    @Override
+    public void keyPressedU() {
+
+    }
+
+    @Override
+    public void keyPressedW() {
+
+    }
+
+    @Override
+    public void keyPressedE() {
+
+    }
+
+    @Override
+    public void keyPressed4() {
+
+    }
+
+    @Override
+    public void keyPressed6() {
+
+    }
+
+    @Override
+    public void keyPressedEnter() {
+
     }
 }
