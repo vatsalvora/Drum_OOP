672208f5ffa4d3e5621f8c6a2216f7684e649033
devan <devanp92@ufl.edu>
2014-04-10 17:42:17 -0400

    fixing compiler errors

--- src/model/Start.java
+++ src/model/Start.java
@@ -3,11 +3,130 @@ package model;
 /**
  * Created by devan on 4/9/14.
  */
-public class Start extends State {
+public class Start implements State {
+
     @Override
-    public void acceptInput(char c) {
-        switch (c){
-            case 'r': //create new game
-        }
+    public void changeCurrentState(State state) {
+
+    }
+
+    @Override
+    public void changeState(StateType stateType) {
+
+    }
+
+    @Override
+    public StateType getCurrentState() {
+        return null;
+    }
+
+    @Override
+    public void incorrectKeyPressed() {
+
+    }
+
+    @Override
+    public void keyPressed1() {
+
+    }
+
+    @Override
+    public void keyPressed2() {
+
+    }
+
+    @Override
+    public void keyPressed3() {
+
+    }
+
+    @Override
+    public void keyPressed7() {
+
+    }
+
+    @Override
+    public void keyPressed8() {
+
+    }
+
+    @Override
+    public void keyPressed9() {
+
+    }
+
+    @Override
+    public void keyPressedTab() {
+
+    }
+
+    @Override
+    public void keyPressedR() {
+
+    }
+
+    @Override
+    public void keyPressedP() {
+
+    }
+
+    @Override
+    public void keyPressedV() {
+
+    }
+
+    @Override
+    public void keyPressedI() {
+
+    }
+
+    @Override
+    public void keyPressedX() {
+
+    }
+
+    @Override
+    public void keyPressedA() {
+
+    }
+
+    @Override
+    public void keyPressedESC() {
+
+    }
+
+    @Override
+    public void keyPressedF() {
+
+    }
+
+    @Override
+    public void keyPressedU() {
+
+    }
+
+    @Override
+    public void keyPressedW() {
+
+    }
+
+    @Override
+    public void keyPressedE() {
+
+    }
+
+    @Override
+    public void keyPressed4() {
+
+    }
+
+    @Override
+    public void keyPressed6() {
+
+    }
+
+    @Override
+    public void keyPressedEnter() {
+
     }
 }
