b75b772a6fbced36f2de7dfd36b726342b67bd68
devan <devanp92@ufl.edu>
2014-04-13 15:52:09 -0400

    Moving this to new subdirectory

---
+++ src/model/state/SharedActionsPlanningAndTurn.java
@@ -0,0 +1,61 @@
+package model.state;
+
+/**
+ * Created by devan on 4/10/14.
+ */
+public abstract class SharedActionsPlanningAndTurn implements State {
+    @Override
+    public void keyPressed1() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressed2() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressed3() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressed7() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressed8() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressed9() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressedTab() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressedR() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressedP() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressedV() {
+        // need to implement
+    }
+
+    @Override
+    public void keyPressedEnter() {
+        // need to implement
+    }
+}
