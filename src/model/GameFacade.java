71e5bd468ac37ffb1f0437d4d47059a54549ebfa
devan <devanp92@ufl.edu>
2014-04-13 19:27:22 -0400

    Same

--- src/model/GameFacade.java
+++ src/model/GameFacade.java
@@ -1,36 +1,36 @@
 package model;
 
 public class GameFacade {
-	BoardController b;
-	TurnController t;
-	SharedResourcesController s;
+	BoardController boardController;
+	TurnController turnController;
+	SharedResourcesController sharedResourcesController;
 
 	public GameFacade(String[] player) {
-		t = new TurnController(player);
-		b = new BoardController(new Board());
-		s = new SharedResourcesController();
+		turnController = new TurnController(player);
+		boardController = new BoardController(new Board());
+		sharedResourcesController = new SharedResourcesController();
 	}
 
-	public void placeIrrigationTile(Location l) {
+	public void placeIrrigationTile(Location location) {
 	}
 
-	public void placeVillageTile(Location l) {
+	public void placeVillageTile(Location location) {
 	}
 
-	public void placeRiceTile(Location l) {
+	public void placeRiceTile(Location location) {
 	}
 
-	public void placeDoubleLandTile(Location l) {
+	public void placeDoubleLandTile(Location location) {
 	}
 
-	public void placeTripleLandTile(Location l) {
+	public void placeTripleLandTile(Location location) {
 	}
 
 	public void initiatePalaceFestival() {
 	}
 
 	public void changeTurn() {
-		t.nextTurn();
+		turnController.nextTurn();
 	}
 
 	public void endPalaceFestival() {
