2771f25f497e4f63057b6a813a882f7d09dc68e8
devan <devanp92@ufl.edu>
2014-04-13 18:16:35 -0400

    reformatting name

--- src/model/CommandCreator.java
+++ src/model/CommandCreator.java
@@ -4,56 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommandCreator {
-	private GameFacade b;
+    private GameFacade b;
 
-	private List<Command> commands = new ArrayList<Command>();
+    private List<Command> commands = new ArrayList<Command>();
 
-	public CommandCreator(GameFacade b) {
-		this.b = b;
-	}
+    public CommandCreator(GameFacade b) {
+        this.b = b;
+    }
 
-	public void PlaceDoubleLandTile(Location l) {
-		Command c = new PlaceDoubleLandTile(b, l);
-		c.execute();
-		commands.add(c);
-	}
+    public void placeDoubleLandTile(Location l) {
+        Command c = new PlaceDoubleLandTile(b, l);
+        c.execute();
+        commands.add(c);
+    }
 
-	public void PlaceTripleLandTile(Location l) {
-		Command c = new PlaceTripleLandTile(b, l);
-		c.execute();
-		commands.add(c);
-	}
+    public void placeTripleLandTile(Location l) {
+        Command c = new PlaceTripleLandTile(b, l);
+        c.execute();
+        commands.add(c);
+    }
 
-	public void PlaceIrrigationTile(Location l) {
-		Command c = new PlaceIrrigationTile(b, l);
-		c.execute();
-		commands.add(c);
-	}
+    public void placeIrrigationTile(Location l) {
+        Command c = new PlaceIrrigationTile(b, l);
+        c.execute();
+        commands.add(c);
+    }
 
-	public void PlaceRiceTile(Location l) {
-		Command c = new PlaceRiceTile(b, l);
-		c.execute();
-		commands.add(c);
-	}
+    public void placeRiceTile(Location l) {
+        Command c = new PlaceRiceTile(b, l);
+        c.execute();
+        commands.add(c);
+    }
 
-	public void InitiatePalaceFestival() {
-		Command c = new InitiatePalaceFestival(b);
-		c.execute();
-		commands.add(c);
-	}
+    public void initiatePalaceFestival() {
+        Command c = new InitiatePalaceFestival(b);
+        c.execute();
+        commands.add(c);
+    }
 
-	public void changeTurn() {
-		Command c = new ChangeTurn(b);
-		c.execute();
-		commands.add(c);
-	}
+    public void changeTurn() {
+        Command c = new ChangeTurn(b);
+        c.execute();
+        commands.add(c);
+    }
 
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
 
-		for (Command c : commands) {
-			sb.append(c.getClass() + "\n");
-		}
-		return sb.toString();
-	}
+        for (Command c : commands) {
+            sb.append(c.getClass() + "\n");
+        }
+        return sb.toString();
+    }
 }
