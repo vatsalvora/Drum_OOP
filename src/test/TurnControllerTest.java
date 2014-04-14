ecd9ac5e4d40f6f8877f5a41d4bf5cb21084725c
devan <devanp92@ufl.edu>
2014-04-13 20:02:29 -0400

    Creating new test package

---
+++ src/test/TurnControllerTest.java
@@ -0,0 +1,192 @@
+package test;
+
+
+import org.junit.Test;
+
+/**
+ * Created by devan on 4/13/14.
+ */
+public class TurnControllerTest {
+    String[] names = {"Joe", "Chantelle", "Destiny", "Laqueesha"};
+
+    
+    @Test
+    public void testAPLeft() throws Exception {
+
+    }
+
+    @Test
+    public void testPlayedBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testTokenUsed() throws Exception {
+
+    }
+
+    @Test
+    public void testNextTurn() throws Exception {
+
+    }
+
+    @Test
+    public void testPreviousTurn() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayerName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayerColor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentCards() throws Exception {
+
+    }
+
+    @Test
+    public void testIncrementFamePoints() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrementFamePoints() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceDeveloper() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveDeveloper() throws Exception {
+
+    }
+
+    @Test
+    public void testUseActionToken() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnActionToken() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceRiceBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnRiceBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceVillageBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnVillageBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceTwoBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnTwoBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testAddCard() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawCard() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawFestivalCard() throws Exception {
+
+    }
+
+    @Test
+    public void testPlaceOtherBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnOtherBlock() throws Exception {
+
+    }
+
+    @Test
+    public void testPerformAction() throws Exception {
+
+    }
+
+    @Test
+    public void testUndoAction() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFestival() throws Exception {
+
+    }
+
+    @Test
+    public void testStartFestival() throws Exception {
+
+    }
+
+    @Test
+    public void testChangeFestivalPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void testPlayCard() throws Exception {
+
+    }
+
+    @Test
+    public void testFreeseFestivalPlayer() throws Exception {
+
+    }
+
+    @Test
+    public void testGetVictors() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentFestivalPlayer() throws Exception {
+
+    }
+}
