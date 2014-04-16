9d65f8417afd9d9fa38b6b22410e0b422f80de0d
devan <devanp92@ufl.edu>
2014-04-16 19:34:33 -0400

    Adding irrigation tiles

--- src/model/SharedResources.java
+++ src/model/SharedResources.java
@@ -1,43 +1,54 @@
 package model;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class SharedResources {
-	ArrayList<ThreeBlock> threes;
-	ArrayList<Integer> palaceTiles;
+    ArrayList<ThreeBlock> threes;
+    ArrayList<Integer> palaceTiles;
+    ArrayList<IrrigationTile> irrigationTiles;
 
-	public SharedResources() {
-		threes = new ArrayList<ThreeBlock>();
-		palaceTiles = new ArrayList<Integer>();
+    public SharedResources() {
+        threes = new ArrayList<ThreeBlock>();
+        palaceTiles = new ArrayList<Integer>();
+        irrigationTiles = new ArrayList<IrrigationTile>();
 
-		for (int i = 0; i < 56; i++)
-			;
-		// add 56 three tiles
-		for (int i = 0; i < 16; i++)
-			;
-		// add 16 Irrigation Tiles
-	}
+        // add 56 three tiles
+        for (int i = 0; i < 56; i++) {
+            //TODO Please make sure this is right and updated after implmenting three block tile
+            threes.add(new ThreeBlock());
+        }
 
-	public SharedResources(int thr, int irr, int[] pt) {
-		threes = new ArrayList<ThreeBlock>();
-		palaceTiles = new ArrayList<Integer>();
+        // add 16 Irrigation Tiles
+        for (int i = 0; i < 16; i++) {
+            //TODO Need to add num neighbors to irrigation tiles
+//        irrigationTiles.add(new IrrigationTile());
+        }
 
-		for (int i = 0; i < thr; i++)
-			threes.add(new ThreeBlock());
 
-		for (int i = 0; i < irr; i++)
-			;
-		// irrigations.add(new OneBlock(TileType.IRRIGATION));
+    }
 
-		for (int i = 0; i < pt.length; i++)
-			palaceTiles.add(pt[i]);
-	}
+    public SharedResources(int thr, int irr, int[] pt) {
+        threes = new ArrayList<ThreeBlock>();
+        palaceTiles = new ArrayList<Integer>();
 
-	public int numThreeBlocks() {
-		return threes.size();
-	}
+        for (int i = 0; i < thr; i++)
+            threes.add(new ThreeBlock());
 
-	public int numPalaceTiles(int level) {
-		return palaceTiles.get(level);
-	}
+        for (int i = 0; i < irr; i++) {
+        }
+
+        // irrigations.add(new OneBlock(TileType.IRRIGATION));
+
+        for (int aPt : pt) {
+            palaceTiles.add(aPt);
+        }
+    }
+
+    public int numThreeBlocks() {
+        return threes.size();
+    }
+
+    public int numPalaceTiles(int level) {
+        return palaceTiles.get(level);
+    }
 }
