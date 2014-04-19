38e0d87ced06baacfffc770957da18b3e284ecb7
xstream587 <mannycarr587@ufl.edu>
2014-04-19 11:06:42 -0400

    commit i forgot about yesterday

--- src/model/GameFacade.java
+++ src/model/GameFacade.java
@@ -4,6 +4,7 @@ import controller.BoardController;
 import controller.SharedResourcesController;
 import controller.TurnController;
 import model.customExceptions.BlockNotPlayedException;
+import model.customExceptions.NoIrrigationLeftException;
 
 public class GameFacade {
 	BoardController boardController;
@@ -17,6 +18,22 @@ public class GameFacade {
 	}
 
 	public void placeIrrigationTile(Location location) {
+        try
+        {
+            sharedResourcesController.placeIrrigationTile();
+            try
+            {
+                //place the irrigation at the proper spot
+            }
+            catch(Exception e)
+            {
+                //return irrigation tile
+            }
+        }
+        catch(NoIrrigationLeftException e)
+        {
+            //possibly do something with the exception, such as print it to the user
+        }
 	}
 
 	public void placeVillageTile(Location location) {
