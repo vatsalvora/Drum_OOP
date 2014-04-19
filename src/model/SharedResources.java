6301e50869a08b3df0a870c4d06a4a8b7d201cf8
xstream587 <mannycarr587@ufl.edu>
2014-04-18 21:10:14 -0400

    shared resources modified and extended

--- src/model/SharedResources.java
+++ src/model/SharedResources.java
@@ -1,20 +1,28 @@
 package model;
 
+import model.customExceptions.NoPalaceTilesLeft;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SharedResources {
     private Deck deck;
     private int numIrrigationTiles;
     private int numThreeBlockTiles;
-    private Map<PalaceLevel, Integer> numTilesInPalaceLevel;
+    private int levelTwoPalace;
+    private int levelFourPalace;
+    private int levelSixPalace;
+    private int levelEightPalace;
+    private int levelTenPalace;
 
     public SharedResources() {
         this.deck = new Deck();
         this.numIrrigationTiles = 16;
         this.numThreeBlockTiles = 56;
-        this.numTilesInPalaceLevel = new HashMap<PalaceLevel, Integer>();
+        levelTwoPalace = 6;
+        levelFourPalace = 7;
+        levelSixPalace = 8;
+        levelEightPalace = 9;
+        levelTenPalace = 10;
     }
 
     public Deck getDeck() {
@@ -33,12 +41,125 @@ public class SharedResources {
         return numThreeBlockTiles;
     }
 
-    public int getNumTilesInPalaceLevel(PalaceLevel level) {
-        return numTilesInPalaceLevel.get(level);
+    public int getNumTwoPalaces() {
+        return levelTwoPalace;
     }
 
-    public void addTileToPalaceLevel(PalaceLevel level) {
-        int numTile = this.numTilesInPalaceLevel.get(level);
-        this.numTilesInPalaceLevel.put(level, ++numTile);
+    public int getNumFourPalaces()
+    {
+        return levelFourPalace;
+    }
+
+    public int getNumSixPalaces()
+    {
+        return levelSixPalace;
+    }
+
+    public int getNumEightPalaces()
+    {
+        return levelEightPalace;
+    }
+
+    public int getNumTenPalaces()
+    {
+        return levelTenPalace;
+    }
+
+    public void placeIrrigationTile()
+    {
+        if(numIrrigationTiles > 0)
+        {
+            numIrrigationTiles--;
+        }
+        else
+        {
+            //put-in: no irrigation tiles left
+        }
+    }
+
+    public void placeThreeBlock()
+    {
+        if(numThreeBlockTiles > 0)
+        {
+            numThreeBlockTiles--;
+        }
+        else
+        {
+            //put-in: no three block tiles left
+        }
+    }
+
+    public void placePalaceTile(int i) throws NoPalaceTilesLeft
+    {
+        switch(i)
+        {
+            case 2:
+                if(levelTwoPalace > 0)
+                {
+                    levelTwoPalace--;
+                }
+                else
+                {
+                    throw new NoPalaceTilesLeft("2");
+                }
+                break;
+            case 4:
+                if(levelFourPalace > 0)
+                {
+                    levelFourPalace--;
+                }
+                else
+                {
+                    throw new NoPalaceTilesLeft("4");
+                }
+                break;
+            case 6:
+                if(levelSixPalace > 0)
+                {
+                    levelSixPalace--;
+                }
+                else
+                {
+                    throw new NoPalaceTilesLeft("6");
+                }
+                break;
+            case 8:
+                if(levelEightPalace > 0)
+                {
+                    levelEightPalace--;
+                }
+                else
+                {
+                    throw new NoPalaceTilesLeft("8");
+                }
+                break;
+            case 10:
+                if(levelTenPalace > 0)
+                {
+                    levelTenPalace--;
+                }
+                else
+                {
+                    throw new NoPalaceTilesLeft("10");
+                }
+                break;
+            default:
+                throw new NoPalaceTilesLeft("" + i + "");
+        }
+    }
+
+    public PalaceCard drawCard()
+    {
+        return deck.drawCard();
+    }
+
+    public void discardCard(PalaceCard c)
+    {
+        deck.discardCard(c);
+    }
+
+    public void returnCard(PalaceCard c)
+    {
+        deck.returnCard(c);
     }
 }
