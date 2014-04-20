cf04a466c85b826cce367adb05572f67cb4cdd8c
jgpradoa <josegpradoa@gmail.com>
2014-04-19 22:45:10 -0400

    I updated Tile

--- src/model/Tile.java
+++ src/model/Tile.java
@@ -8,8 +8,12 @@ public abstract class Tile {
 
 
 	public abstract boolean compareTo(Tile t);
-	//public abstract void assignPossition(int possiton);
-	//public abstract int getPossition();
     public abstract Color getColor();
+    public abstract void assignNumberOfNeighbors(int numberOfNeighbors);
+    public abstract void initNeighbors();
+    public abstract void createReff(Tile tile, int index);
+    public abstract void removeReff(int index);
+    public abstract void assignColor(int a ,int b,int c);
+
 
 }
